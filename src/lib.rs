794f93dd8096ad22615a49fe22e93200ada19c1b
ryan <ryan@kurte.nz>
2021-02-08 21:38:08 +1300

    working on sensor reliability doesn't _seem_ to fail on rpi, needs further investigation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #![no_std]
 
 /// Rust tlv493d 3-DoF I2C Hall Effect Sensor Driver
@@ -7,23 +6,24 @@
 ///
 /// - https://www.infineon.com/dgdl/Infineon-TLV493D-A1B6-DataSheet-v01_10-EN.pdf?fileId=5546d462525dbac40152a6b85c760e80
 /// - https://www.infineon.com/dgdl/Infineon-TLV493D-A1B6_3DMagnetic-UM-v01_03-EN.pdf?fileId=5546d46261d5e6820161e75721903ddd
-
-
+use core::fmt::Debug;
 use core::marker::PhantomData;
 
-use log::{debug};
 use bitflags::bitflags;
-use embedded_hal::blocking::i2c;
+use embedded_hal::blocking::{delay, i2c};
+use log::debug;
 
 #[cfg(feature = "std")]
 extern crate std;
 
-pub struct Tlv493d<I2c, E> {
+pub struct Tlv493d<I2c, I2cErr, Delay, DelayErr> {
     i2c: I2c,
+    _delay: Delay,
     addr: u8,
     initial: [u8; 10],
     last_frm: u8,
-    _e: PhantomData<E>,
+    _e_i2c: PhantomData<I2cErr>,
+    _e_delay: PhantomData<DelayErr>,
 }
 
 /// Base address for Tlv493d, bottom bit set during power up
@@ -32,14 +32,14 @@ pub const ADDRESS_BASE: u8 = 0b1011110;
 
 /// Read registers for the Tlv493d
 pub enum ReadRegisters {
-    Rx      = 0x00, // X direction flux (Bx[11..4])
-    By      = 0x01, // Y direction flux (By[11..4])
-    Bz      = 0x02, // X direction flux (Bz[11..4])
-    Temp    = 0x03, // Temperature high bits, frame counter, channel, (Temp[11..8] | FRM[1..0] | CH[1..0])
-    Bx2     = 0x04, // Lower X and Y flux (Bx[3..0] | Bx[3..0])
-    Bz2     = 0x05, // Flags + Lower Z flux (Reserved | T | FF | PD | Bz[3..0])
-    Temp2   = 0x06, // Temperature low bits (Temp[7..0])
-    
+    Rx = 0x00,    // X direction flux (Bx[11..4])
+    By = 0x01,    // Y direction flux (By[11..4])
+    Bz = 0x02,    // X direction flux (Bz[11..4])
+    Temp = 0x03, // Temperature high bits, frame counter, channel, (Temp[11..8] | FRM[1..0] | CH[1..0])
+    Bx2 = 0x04,  // Lower X and Y flux (Bx[3..0] | Bx[3..0])
+    Bz2 = 0x05,  // Flags + Lower Z flux (Reserved | T | FF | PD | Bz[3..0])
+    Temp2 = 0x06, // Temperature low bits (Temp[7..0])
+
     FactSet1 = 0x07,
     FactSet2 = 0x08,
     FactSet3 = 0x09,
@@ -47,29 +47,29 @@ pub enum ReadRegisters {
 
 /// Write registers for the Tlv493d
 pub enum WriteRegisters {
-    Res     = 0x00, // Reserved
-    Mode1   = 0x01, // Mode 1 register (P | IICAddr[1..0] | Reserved[1..0] | INT | FAST | LOW)
-    Res2    = 0x02, // Reserved
-    Mode2   = 0x03, // Mode 2 register (T | LP | PT | Reserved[4..0])
+    Res = 0x00,   // Reserved
+    Mode1 = 0x01, // Mode 1 register (P | IICAddr[1..0] | Reserved[1..0] | INT | FAST | LOW)
+    Res2 = 0x02,  // Reserved
+    Mode2 = 0x03, // Mode 2 register (T | LP | PT | Reserved[4..0])
 }
 
 /// TLV493D Measurement values
 #[derive(Debug, PartialEq, Clone)]
 pub struct Values {
-    x: f32,     // X axis magnetic flux (mT)
-    y: f32,     // Y axis magnetic flux (mT)
-    z: f32,     // Z axis magnetic flux (mT)
-    temp: f32,  // Device temperature (C)
+    x: f32,    // X axis magnetic flux (mT)
+    y: f32,    // Y axis magnetic flux (mT)
+    z: f32,    // Z axis magnetic flux (mT)
+    temp: f32, // Device temperature (C)
 }
 
 /// Device operating mode
 /// Note that in most cases the mode is a combination of mode and IRQ flags
 pub enum Mode {
-    Disabled,       // Reading disabled
-    Master,         // Master initiated mode (reading occurs after readout)
-    Fast,           // Fast mode (3.3kHz)
-    LowPower,       // Low power mode (100Hz)
-    UltraLowPower,  // Ultra low power mode (10Hz)
+    Disabled,      // Reading disabled
+    Master,        // Master initiated mode (reading occurs after readout)
+    Fast,          // Fast mode (3.3kHz)
+    LowPower,      // Low power mode (100Hz)
+    UltraLowPower, // Ultra low power mode (10Hz)
 }
 
 bitflags! {
@@ -93,65 +93,99 @@ bitflags! {
     }
 }
 
-
 /// Tlv493d related errors
 #[derive(Debug)]
-#[cfg_attr(feature = "std", derive(thiserror::Error))] 
-pub enum Error<E: core::fmt::Debug> {
+#[cfg_attr(feature = "std", derive(thiserror::Error))]
+pub enum Error<I2cErr: Debug, DelayErr: Debug> {
     // No device found with specified i2c bus and address
-    #[cfg_attr(feature = "std", error("No device found with specified i2c bus and address"))] 
+    #[cfg_attr(
+        feature = "std",
+        error("No device found with specified i2c bus and address")
+    )]
     NoDevice,
 
     // Device ADC locked up and must be reset
-    #[cfg_attr(feature = "std", error("Device ADC lockup, reset required"))] 
+    #[cfg_attr(feature = "std", error("Device ADC lockup, reset required"))]
     AdcLockup,
 
     // Underlying I2C device error
-    #[cfg_attr(feature = "std", error("I2C device error: {0:?}"))] 
-    I2c(E),
+    #[cfg_attr(feature = "std", error("I2C device error: {0:?}"))]
+    I2c(I2cErr),
+
+    // Underlying delay driver error
+    #[cfg_attr(feature = "std", error("Delay error: {0:?}"))]
+    Delay(DelayErr),
 }
 
-impl <I2c, E> Tlv493d<I2c, E>
-where 
-    I2c: i2c::Read<Error = E> + i2c::Write<Error = E> + i2c::WriteRead<Error = E>,
-    E: core::fmt::Debug,
+impl<I2c, I2cErr, Delay, DelayErr> Tlv493d<I2c, I2cErr, Delay, DelayErr>
+where
+    I2c: i2c::Read<Error = I2cErr> + i2c::Write<Error = I2cErr> + i2c::WriteRead<Error = I2cErr>,
+    I2cErr: Debug,
+    Delay: delay::DelayMs<u32, Error = DelayErr>,
+    DelayErr: Debug,
 {
     /// Create a new TLV493D instance
-    pub fn new(i2c: I2c, addr: u8, mode: Mode) -> Result<Self, Error<E>> {
+    pub fn new(
+        i2c: I2c,
+        delay: Delay,
+        addr: u8,
+        mode: Mode,
+    ) -> Result<Self, Error<I2cErr, DelayErr>> {
         debug!("New Tlv493d with address: 0x{:02x}", addr);
-        
+
         // Construct object
         let mut s = Self {
-            i2c, addr, initial: [0u8; 10], last_frm: 0xff, _e: PhantomData,
+            i2c,
+            _delay: delay,
+            addr,
+            initial: [0u8; 10],
+            last_frm: 0xff,
+            _e_i2c: PhantomData,
+            _e_delay: PhantomData,
         };
 
-        // Startup per fig. 5.1 in TLV493D-A1B6 user manual
-
-        // Write recovery value
-        s.i2c.write(s.addr, &[0xFF]).map_err(Error::I2c)?;
-
-        // Send reset command
-        // TODO: this does not appear to work?
-        s.i2c.write(s.addr, &[0x00]).map_err(Error::I2c)?;
-       
-        // Read initial bitmap from device
-        let _ = s.i2c.read(s.addr, &mut s.initial[..]).map_err(Error::I2c)?;
-
-        debug!("initial read: {:02x?}", s.initial);
-
-        // Set mode
-        s.configure(mode)?;
+        // Reset and configure
+        s.configure(mode, true)?;
 
         // Return object
         Ok(s)
     }
 
-    pub fn configure(&mut self, mode: Mode) -> Result<(), Error<E>> {
+    /// Configure the device into the specified mode
+    pub fn configure(&mut self, mode: Mode, reset: bool) -> Result<(), Error<I2cErr, DelayErr>> {
+        // Startup per fig. 5.1 in TLV493D-A1B6 user manual
 
+        // Reset if enabled
+        if reset {
+            debug!("Resetting device");
+
+            // Write recovery value
+            self.i2c.try_write(self.addr, &[0xFF]).map_err(Error::I2c)?;
+
+            // Wait for startup delay
+            self._delay.try_delay_ms(40).map_err(Error::Delay)?;
+
+            debug!("Setting device address");
+
+            // TODO: work out why things get upset if we set the address
+            //self.i2c.write(0x00, &[0xFF]).map_err(Error::I2c)?;
+
+            debug!("Read device initial state");
+
+            // Read initial bitmap from device
+            let _ = self
+                .i2c
+                .try_read(self.addr, &mut self.initial[..])
+                .map_err(Error::I2c)?;
+
+            debug!("Initial state: {:02x?}", self.initial);
+        }
+
+        // Parse out initial mode settings
         let mut m1 = unsafe { Mode1::from_bits_unchecked(self.initial[7]) };
         let m2 = unsafe { Mode2::from_bits_unchecked(self.initial[9]) };
 
-        debug!("Factory config: {:?} ({:02x?})", m1, self.initial);
+        debug!("Current config: {:?} ({:02x?})", m1, self.initial);
 
         // Clear mode flags
         m1.remove(Mode1::PARITY);
@@ -159,18 +193,16 @@ where
 
         match mode {
             Mode::Disabled => (),
-            Mode::Master            => m1 |= Mode1::FAST | Mode1::LOW,
-            Mode::Fast              => m1 |= Mode1::FAST | Mode1::IRQ_EN,
-            Mode::LowPower          => m1 |= Mode1::LOW | Mode1::IRQ_EN,
-            Mode::UltraLowPower     => m1 |= Mode1::IRQ_EN,
+            Mode::Master => m1 |= Mode1::FAST | Mode1::LOW,
+            Mode::Fast => m1 |= Mode1::FAST | Mode1::IRQ_EN,
+            Mode::LowPower => m1 |= Mode1::LOW | Mode1::IRQ_EN,
+            Mode::UltraLowPower => m1 |= Mode1::IRQ_EN,
         }
 
-        let mut cfg = [
-            0x00,
-            m1.bits(),
-            self.initial[8],
-            m2.bits(),
-        ];
+        let mut cfg = [0x00, m1.bits(), self.initial[8], m2.bits()];
+
+        self.initial[7] = m1.bits();
+        self.initial[9] = m2.bits();
 
         let mut parity = 0;
         for v in &cfg {
@@ -185,29 +217,34 @@ where
             cfg[1] = m1.bits();
         }
 
-        debug!("Writing config: Mode1: {:?} Mode2: {:?} (cfg: {:02x?}", m1, m2, cfg);
+        debug!(
+            "Writing config: Mode1: {:?} Mode2: {:?} (cfg: {:02x?}",
+            m1, m2, cfg
+        );
 
-        self.i2c.write(self.addr, &cfg).map_err(Error::I2c)?;
+        self.i2c.try_write(self.addr, &cfg).map_err(Error::I2c)?;
 
         Ok(())
     }
 
     /// Read raw values from the sensor
-    pub fn read_raw(&mut self) -> Result<[i16; 4], Error<E>> {
+    pub fn read_raw(&mut self) -> Result<[i16; 4], Error<I2cErr, DelayErr>> {
         let mut v = [0i16; 4];
 
         // Read data from device
         let mut b = [0u8; 7];
-        self.i2c.read(self.addr, &mut b[..]).map_err(Error::I2c)?;
+        self.i2c
+            .try_read(self.addr, &mut b[..])
+            .map_err(Error::I2c)?;
 
         // Detect ADC lockup (stalled FRM field)
         let frm = b[3] & 0b0000_1100;
         if self.last_frm == frm {
-            return Err(Error::AdcLockup)
+            return Err(Error::AdcLockup);
         } else {
             self.last_frm = frm;
         }
-        
+
         // Convert to values
         // Double-cast here required for sign-extension
         v[0] = (b[0] as i8 as i16) << 4 | ((b[4] & 0xF0) >> 4) as i16;
@@ -221,7 +258,7 @@ where
     }
 
     /// Read and convert values from the sensor
-    pub fn read(&mut self) -> Result<Values, Error<E>> {
+    pub fn read(&mut self) -> Result<Values, Error<I2cErr, DelayErr>> {
         let raw = self.read_raw()?;
 
         Ok(Values {
@@ -231,5 +268,13 @@ where
             temp: (raw[3] - 340) as f32 * 1.1f32 + 24.2f32,
         })
     }
-}
 
+    #[cfg(feature = "math")]
+    pub fn read_angle_f32(&mut self) -> Result<f32, Error<I2cErr, DelayErr>> {
+        // Read values
+        let v = self.read()?;
+
+        // https://en.wikipedia.org/wiki/Atan2
+        Ok(v.x.atan2(v.y))
+    }
+}
--- src/util.rs
+++ src/util.rs
@@ -1,20 +1,17 @@
-
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
 
-use structopt::StructOpt;
+use log::{debug, info};
 use simplelog::{LevelFilter, SimpleLogger, TermLogger, TerminalMode};
-use log::{info, debug};
+use structopt::StructOpt;
 
-use sensor_tlv493d::{Tlv493d, Mode};
-use linux_embedded_hal::I2cdev;
-
+use linux_embedded_hal::{Delay, I2cdev};
+use sensor_tlv493d::{Mode, Tlv493d};
 
 /// Connect to a TLV493D device and read out values
 #[derive(PartialEq, Clone, Debug, StructOpt)]
 pub struct Options {
-
     #[structopt(long, default_value = "/dev/i2c-1")]
     /// Linux I2C device
     pub i2c_dev: String,
@@ -24,8 +21,16 @@ pub struct Options {
     pub i2c_baud_khz: u32,
 
     #[structopt(long, parse(try_from_str=u8_from_hex), default_value = "5e")]
-    /// Device I2C address in hex 
+    /// Device I2C address in hex
     pub i2c_addr: u8,
+
+    #[structopt(long, default_value = "1")]
+    /// Poll frequency in Hz
+    pub freq_hz: u32,
+
+    #[structopt(long)]
+    /// Output angle in place of raw readings
+    pub angle: bool,
 
     #[structopt(long, default_value = "info")]
     /// Configure app logging levels (warn, info, debug, trace)
@@ -47,35 +52,42 @@ fn main() -> Result<(), anyhow::Error> {
     if let Err(_e) = TermLogger::init(opts.log_level, log_config.clone(), TerminalMode::Mixed) {
         SimpleLogger::init(opts.log_level, log_config).unwrap();
     }
-    
-    info!("Connecting to sensor: 0x{:02x} on bus: {}", opts.i2c_addr, opts.i2c_dev);
+
+    info!(
+        "Connecting to sensor: 0x{:02x} on bus: {}",
+        opts.i2c_addr, opts.i2c_dev
+    );
 
     // Connect to I2C
     let i2c = I2cdev::new(opts.i2c_dev)?;
 
     // Create sensor
-    let mut sensor = Tlv493d::new(i2c, opts.i2c_addr, Mode::Master)?;
+    let mut sensor = Tlv493d::new(i2c, Delay {}, opts.i2c_addr, Mode::Master)?;
 
     // Setup exit handler
     let r = running.clone();
     ctrlc::set_handler(move || {
         r.store(false, Ordering::SeqCst);
-    }).expect("Error setting Ctrl-C handler");
+    })
+    .expect("Error setting Ctrl-C handler");
 
     debug!("Running");
 
     // Read from sensor
     while running.load(Ordering::SeqCst) {
-        
-        let v = sensor.read()?;
+        if opts.angle {
+            let v = sensor.read_angle_f32()?;
+            println!("{:03.04?}", v);
+        } else {
+            let v = sensor.read()?;
+            println!("{:03.04?}", v);
+        }
 
-        info!("{:03.04?}", v);
-
-        std::thread::sleep(Duration::from_secs(1));
+        std::thread::sleep(Duration::from_secs(1) / opts.freq_hz);
     }
 
     // Disable sensor before exit
-    sensor.configure(Mode::Disabled)?;
+    sensor.configure(Mode::Disabled, false)?;
 
     Ok(())
 }
